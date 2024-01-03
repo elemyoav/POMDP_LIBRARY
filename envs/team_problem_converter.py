60edeccc14c0ab631cf0ae486ee2cd5635100d6c
elemyoav <elemyoav@gmail.com>
2024-01-03 08:57:17 +0200

    added the converter interface, needs to be implemented

---
+++ envs/team_problem_converter.py
@@ -0,0 +1,15 @@
+from ray.rllib import MultiAgentEnv
+from gymnasium import Env as GymEnv
+
+
+# TODO: Implement this function
+def convert_to_gym_env(env: MultiAgentEnv) -> GymEnv:
+    """Converts a MultiAgentEnv to a GymEnv.
+
+    Args:
+        env (MultiAgentEnv): The MultiAgentEnv to convert.
+
+    Returns:
+        GymEnv: The converted GymEnv.
+    """
+    pass
--- main.py
+++ main.py
@@ -5,43 +5,46 @@ from ray.tune.registry import get_trainable_cls
 import matplotlib.pyplot as plt
 import numpy as np
 from datetime import datetime
+import argparse
 
-
+Env = DecTigerEnv
+Config = PPOConfig
 EXPERIMENT_NAME = 'PPO_Tiger' + datetime.now().strftime("%Y%m%d-%H%M%S")
 ray.init()
 
-ppo_config = (
-    PPOConfig()\
+
+config = (
+     Config()\
     .framework('torch')\
     .rollouts(create_env_on_local_worker=True)\
     .debugging(seed=0, log_level='ERROR')
     .environment(DecTigerEnv)
 )
 
-ppo_config = ppo_config.multi_agent(
+config = config.multi_agent(
     policies=['policy_0', 'policy_1'],
     policy_mapping_fn=lambda agent_id, _, **kwargs: 'policy_0' if agent_id == 'agent_0' else 'policy_1',
 )
 
-ppo = ppo_config.build()
+alg = config.build()
 rewards1 = []
 rewards2 = []
 
 print("Starting training...")
 for i in range(100):
-    result = ppo.train()
+    result = alg.train()
     rewards1.append(result['policy_reward_mean']['policy_0'])
     rewards2.append(result['policy_reward_mean']['policy_1'])
-    print('episode_reward_mean: ', result['episode_reward_mean'])
+    print('iteration: ', i, ' episode_reward_mean: ', result['episode_reward_mean'])
 
 
-ppo.save(f'./results/checkpoints/{EXPERIMENT_NAME}')
+alg.save(f'./results/checkpoints/{EXPERIMENT_NAME}')
 
 
 print("Done!")
 print("Evaluating the algorithm...")
 print("After running an evaluation of the algorithm, got an avg_reward_per_episode of:"\
-      ,ppo.evaluate()["evaluation"]["episode_reward_mean"])
+      ,alg.evaluate()["evaluation"]["episode_reward_mean"])
 
 plt.figure(figsize=(4,3))
 plt.plot(rewards1, label='agent_0')
