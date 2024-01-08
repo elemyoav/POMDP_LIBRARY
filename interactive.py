f6a92adcdf76d90b845c1b2aa94441afc19a08fd
AmirSoli1 <amirsoli@post.bgu.ac.il>
2024-01-09 00:50:05 +0200

    add interactive menu for tiger

---
+++ interactive.py
@@ -0,0 +1,16 @@
+from envs.box_pushing.box_pushing import DecBoxPushing
+from envs.tiger.tiger import DecTigerEnv
+from envs.rock_sampling.rock_sampling import DecRockSampling
+from envs.team_problem_converter import convert_to_gym_env
+
+env = convert_to_gym_env(DecTigerEnv())
+env.render()
+for i in range(3):
+    done = False
+    env.reset()
+    while(not done):
+        a0 = int(input("Choose action for agent 0: "))
+        a1 = int(input("Choose action for agent 1: "))
+        actions = {'agent_0': a0, 'agent_1': a1}
+        obs, reward, done, trunk, info = env.step(actions)
+        env.render()
